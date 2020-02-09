e4350ae206fb1de222966bfdcbbddcf699efcd70
Jose Maradiaga <josemarad25@gmail.com>
2020-02-09 20:38:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,14 +7,16 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language (name, language)
+def introduction_with_language (name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 def introduction_with_language_optional (name,language = "Ruby")
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
-
+ def favoriteColor (color)
+   puts "favorite #{color}"
+end
 
 
 
